f3848ed1f0cd837e154c352467672e5bf3833485
Oyinloye Kayode <oyinsbiz@gmail.com>
2022-09-30 12:43:24 -0700

    Challenge

---
+++ Week1/Day1/hello.py
@@ -0,0 +1,5 @@
+# Program that asks the user to enter his or her name
+# and respond with a message that says hello to the user
+
+name = input("Enter Your Name:")
+print("Hello", name)
---
+++ Week1/Day1/mailinglist.py
@@ -0,0 +1,8 @@
+# A program that displays my name and complete mailing address
+
+user_details = """OYINLOYE KAYODE
+No7 SALVATION STREET, IRAWO,
+LAGOS STATE,
+NIGERIA
+235034"""
+print(user_details)
